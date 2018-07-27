53032901d2a80d0e41efff08f78f8a76e095c80a
Jean Giard <jgiard@antidot.net>
2018-07-27 09:49:23 +0200

    v0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Pynject',
-      version='0.2',
+      version='0.3',
       description='A minimalist python injector',
       url='https://github.com/JGiard/Pynject',
       author='Jean Giard',
